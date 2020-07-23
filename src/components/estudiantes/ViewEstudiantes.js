1e3f752a51b86b3d6fdcb4c486cb087c26173331
anderson <anderdavid86@hotmail.com>
2020-07-23 15:39:13 -0500

    Se despliega modal en vista ver usuarios

--- src/components/estudiantes/ViewEstudiantes.js
+++ src/components/estudiantes/ViewEstudiantes.js
@@ -5,10 +5,27 @@ import {getEstudiantes,deleteEstudiantes} from '../../redux'
 class ViewEstudiantes extends Component {
     constructor(props) {
         super(props);
-        
+        this.state={
+            modalActive:'',
+            modalMsg :'Esta seguro de eliminar este curso'
+        }
+        this.openModal = this.openModal.bind(this)
+        this.closeModal=this.closeModal.bind(this)
     }
     componentWillMount =()=>{
         this.props.getEstudiantes()
+    }
+    openModal =()=>{
+        console.log('openModal()')
+        this.setState(
+            {modalActive:'is-active'}
+        )
+    }
+    closeModal =()=>{
+        console.log('closeModal')
+        this.setState(
+            {modalActive:''}
+        )
     }
     render() {
         var status = this.props.state.estudiantesResponse.status
@@ -28,7 +45,8 @@ class ViewEstudiantes extends Component {
                                     <div className="buttons">
                                         <button className="button is-success" onClick={()=>{window.location.href='/estudiantes/ver/'+estudiante.id}}>Ver</button>
                                         <button className="button is-info" onClick={()=>{window.location.href='/estudiantes/edit/'+estudiante.id}}>Editar</button>
-                                        <button className="button is-danger" onClick={()=>{this.props.deleteEstudiante(estudiante.id)}}>Eliminar</button>
+                                        {/* <button className="button is-danger" onClick={()=>{this.props.deleteEstudiante(estudiante.id)}}>Eliminar</button> */}
+                                        <button className="button is-danger" onClick={this.openModal}>Eliminar</button>
                                     </div>
                                 </td>
                             </tr>
@@ -37,6 +55,8 @@ class ViewEstudiantes extends Component {
 
         return (
             <div>
+                <p>{JSON.stringify(this.state)}</p>
+                <ViewModal active={this.state.modalActive} closeModal={this.closeModal} msg={this.state.modalMsg}/>
                 {/* {JSON.stringify(this.props.state)} */}
                 <h1 className="title has-text-info">Ver estudiantes</h1>
                 <div className="table-container">
@@ -78,3 +98,28 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 ) (ViewEstudiantes)
+
+function ViewModal(props){
+
+    return(
+        
+        <div className= {`modal ${props.active}`}>
+            <div className="modal-background"></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Alerta</p>
+                    <button className="delete" aria-label="close" onClick={props.closeModal}></button>
+                </header>
+                <section className="modal-card-body">
+                    <p>{props.msg}</p>
+                </section>
+                <footer className="modal-card-foot">
+                    <button className="button is-success">Aceptar</button>
+                    <button className="button is-danger" onClick={props.closeModal}>Cancelar</button>
+                </footer>
+            </div>
+        </div>
+       
+    )
+    
+}
