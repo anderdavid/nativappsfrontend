5647eca24e1e1bd54621826ecaf813d901ae4de3
anderson <anderdavid86@hotmail.com>
2020-07-24 22:20:14 -0500

    Se implementa front top3

--- src/components/funciones/Top3.js
+++ src/components/funciones/Top3.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Top3(props) {
-    return (
-        <div>
-            Top3
-        </div>
-    );
+class Top3 extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title has-text-info">Top 3</h1>
+                <ul>
+                    <li className="has-background-light" style={{marginBottom:"10px",padding:"10px 10px"}}>
+                        <strong>top-nombre</strong> con top-inscritos estudiantes inscritos en los ultimos tres meses
+                    </li>
+                    <li className="has-background-light" style={{marginBottom:"10px",padding:"10px 10px"}}>
+                        <strong>top-nombre</strong> con top-inscritos estudiantes inscritos en los ultimos tres meses
+                    </li>
+                    <li className="has-background-light" style={{marginBottom:"10px",padding:"10px 10px"}}>
+                        <strong>top-nombre</strong> con top-inscritos estudiantes inscritos en los ultimos tres meses
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Top3;
