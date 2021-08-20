9917ea5a6148ef35432825039c756df01d1a19db
arodriguezn <arodriguez@nativapps.co>
2021-08-20 18:09:48 -0500

    Se configura devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-import {createStore,applyMiddleware} from 'redux'
-import rootReducer from './rootReducer'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './rootReducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer,applyMiddleware(logger,thunk))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
 
-export default store
+export default store;
